53c04d7fe4e0baf2c2d4db29603e83736bc673dc
Nikita <nikita112243@gmail.com>
2024-03-24 15:06:48 +0400

    add some variables lesson1:task1

--- task1/Program.cs
+++ task1/Program.cs
@@ -1,1 +1,14 @@
-﻿Console.WriteLine("Hello world");
+﻿//Console.WriteLine("Hello world");
+
+
+using System.Collections.Immutable;
+
+int value = 100;
+Console.WriteLine(value);
+
+double d = 5.55; //дробные части
+char c = '!'; //переменная типа char и ее значение хранятся в одинарных ковычках
+bool b = true;
+Console.WriteLine(d);
+Console.WriteLine(c);
+Console.WriteLine(b);
